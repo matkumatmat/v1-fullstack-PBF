325101de35f700a303f5486ba952a432b23d61dc
matkumatmat <mamattewahyu@gmail.com>
2025-09-05 11:40:56 +0700

    delete an error configguration

--- backend/app/services/exceptions.py
+++
@@ -1,5 +0,0 @@
-class ValidationError(Exception):
-    """Custom exception for validation errors in the service layer."""
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
